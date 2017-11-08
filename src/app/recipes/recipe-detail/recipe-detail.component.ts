be7b183e043be1a00abc9b4a7508489c5ff1cbd2
Natthaphon Phanit <natthaphonp@mathregit.group>
2017-11-08 16:33:49 +0700

    first commit

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class RecipeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
+
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class RecipeItemComponent implements OnInit {
+
+  @Input() recipe: Recipe;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
